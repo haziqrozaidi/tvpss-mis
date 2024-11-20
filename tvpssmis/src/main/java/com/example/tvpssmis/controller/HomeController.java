8f7582c5077be074a0a441fe894bc92cff82953d
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2024-11-20 13:15:09 +0800

    Add dashboard route

--- tvpssmis/src/main/java/com/example/tvpssmis/controller/HomeController.java
+++ tvpssmis/src/main/java/com/example/tvpssmis/controller/HomeController.java
@@ -1,5 +1,17 @@
 package com.example.tvpssmis.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@GetMapping("/resources/dashboard")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("dashboard");
+		return modelAndView;
+	}
 }
